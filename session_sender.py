5320eee922ba0e4aea4ff4a5926164dfde243fc3
michailx <michail.xirouchakis@gmail.com>
2016-10-21 01:08:44 +0200

    Second working version. Complete overhaul on the on the test execution. (Request a new session for each ToS value)

--- session_sender.py
+++ session_sender.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 import socket
 from struct import *
 import sys
@@ -33,12 +32,14 @@ def from_NTP_format(timestamp):
 
 class Listening(threading.Thread):
 
-    samples_table = [[], [], [], [], [], [], [], []]  # Packet RTD ... per IP Precedence value
+    #samples_list = []  # Packet RTD of each sample
 
-    def __init__(self, sock, dst_ip, dst_udp_port):
+    def __init__(self, sock, dst_ip, dst_udp_port, tos=0):
         self.sock = sock
         self.dest_ip = dst_ip  # Added destination IP and Port in order to filter out unwanted packets.
         self.dest_udp_port = dst_udp_port
+        self.tos = tos
+        self.samples_list = []  # Packet RTD of each sample
         threading.Thread.__init__(self)
 
     def unauthenticated_response_packet(self, received_data):
@@ -59,64 +60,43 @@ class Listening(threading.Thread):
 
         return packet_header
 
-    def handle_sample(self, sample):
-        # sample is a tuple of two integers: (Sender Sequence Number, Round Trip Delay)
+    def get_stats(self):
+        round_trip_delay = 0.0
+        st_dev = 0.0
 
-        ip_precedence = sample[0] % 8
-        self.samples_table[ip_precedence].append(sample[1])
+        packets_rcved = len(self.samples_list)
+        packets_sent = 0
+        packet_loss = 0
 
-    def get_stats(self, last_sent_sample):
-        round_trip_delay = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-        st_dev = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        # Calculate statistical data: rtd is mean value whereas st_dev is the standard deviation of rtd
+        if packets_rcved == 0:  # If I did not receive a singe TWAMP-Test packet
+            round_trip_delay = 60.0
+            st_dev = 0.0
+        else:
+            round_trip_delay = mean(self.samples_list)
+            st_dev = pstdev(self.samples_list, round_trip_delay)
 
-        packets_rcved = [0, 0, 0, 0, 0, 0, 0, 0]
-        packets_sent = [0, 0, 0, 0, 0, 0, 0, 0]
-        packet_loss = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-
-        for samples in enumerate(self.samples_table):  # Enumerate returns tuple (key,value) of the list
-            # Get info about how many packets have been received (samples)
-            packets_rcved[samples[0]] = len(samples[1])
-
-            # Calculate statistical data: rtd is mean value whereas st_dev is the standard deviation of rtd
-            if len(samples[1]) == 0:  # This occurs when I don't receive a single packet for this IPP during the window
-                round_trip_delay[samples[0]] = 60.0  # Picked a random value, 60 seconds is super high RTD
-                st_dev[samples[0]] = 0.0
-            else:
-                round_trip_delay[samples[0]] = mean(samples[1])
-                st_dev[samples[0]] = pstdev(samples[1], round_trip_delay[samples[0]])
-
-        #print(packets_rcved)
 
         # Get info about packets_sent by "Sending" thread
         while not packets_sent_queue.empty():
             queue_item = packets_sent_queue.get(block=False)
-            packets_sent[int(queue_item) % 8] += 1
-            if last_sent_sample == queue_item:
-                break
+            #print(queue_item)
+            packets_sent += 1
 
+        if packets_sent == 0:
+            print(self.getName() + ': I found no packets in Queue which is fishy!')
+
+
+        #print(packets_rcved)
         #print(packets_sent)
 
-        for ip_precedence in range(0, 8):  # Loop runs for IP Precedence 0 - 7
-            # Explanation: packet_loss = 1 - num_of_packets_received / num_of_packets_sent
-            if packets_sent[ip_precedence] == 0:
-                print(self.getName() + ': I found no packets in Queue for IP Precedence', ip_precedence,
-                      'which is fishy!')
-                continue
-            packet_loss[ip_precedence] = 1 - packets_rcved[ip_precedence] / packets_sent[ip_precedence]
+        packet_loss = 1 - packets_rcved / packets_sent
 
         return packet_loss, round_trip_delay, st_dev
 
-    def clear_stats(self):
-        self.samples_table = [[], [], [], [], [], [], [], []]
-
     def run(self):
-        sample = (0, 0)
-        highest_sample_per_ipp = [0, 0, 0, 0, 0, 0, 0, 0]
-        window_end = 0
-
-        start_time = time.time() + 2208988800
-        current_time = start_time  # Initialise variable here so it is visible within the exception block. Value will
-        # be overwritten in try block.
+        current_time = time.time() + 2208988800 # Initialise variable here so it is visible within the exception block.
+        # Value will be overwritten in try block. (Unless not a single packet arrives)
 
         while True:
             try:
@@ -130,64 +110,43 @@ class Listening(threading.Thread):
                     continue
 
                 header = self.unauthenticated_response_packet(received_data)
+                #print(header['Sender Sequence Number'])
 
-                if header['Sender Sequence Number'] < window_end:
-                    #print('Received re-ordered packet with Sender Sequence Number', header['Sender Sequence Number'],
-                    #      'which I will disregard.')
+                if header['Sender Sequence Number'] < 1000000 * self.tos:
+                    print('Received re-ordered packet with Sender Sequence Number', header['Sender Sequence Number'],
+                          'which I will disregard.')
                     continue
 
                 current_time = time.time() + 2208988800
-
-                sample = (header['Sender Sequence Number'], current_time - header['Sender Timestamp'])
-                self.handle_sample(sample)
-
-                if highest_sample_per_ipp[header['Sender Sequence Number'] % 8] < header['Sender Sequence Number']:
-                    highest_sample_per_ipp[header['Sender Sequence Number'] % 8] = header['Sender Sequence Number']
-
-                #if current_time - start_time >= 30.0 and header['Sender Sequence Number'] % 8 == 5:
-                if current_time - start_time >= 30.0:
-                    window_end = max(highest_sample_per_ipp)  # Highest received Sender Seq Nbr regardless IPP
-                    packet_loss, round_trip_delay, st_dev = self.get_stats(window_end)
-
-                    # Print to terminal:
-                    print(time.strftime("%H:%M:%S", time.gmtime(current_time - 2208988800)), 'UTC >',
-                          'Loss %:', [format(100*x, '.4g') for x in packet_loss],
-                          ', RTD ms:', [format(1000*x, '.4g') for x in round_trip_delay],
-                          '> st.dev ms:', [format(1000*x, '.4g') for x in st_dev]
-                          )
-
-                    # Clear lists
-                    self.clear_stats()
-
-                    # Set new start time
-                    start_time = time.time() + 2208988800
+                self.samples_list.append(current_time - header['Sender Timestamp'])
 
                 # Uncomment following sentence for debug purposes only
                 #print(header)
 
             except socket.timeout:
-                packet_loss, round_trip_delay, st_dev = self.get_stats(sample)
+                packet_loss, round_trip_delay, st_dev = self.get_stats()
 
                 # Print to terminal:
                 # Here current_time is the "timestamp" of the last received packet
                 # Print to terminal:
                 print(time.strftime("%H:%M:%S", time.gmtime(current_time - 2208988800)), 'UTC >',
-                      'Loss %:', [format(100 * x, '.4g') for x in packet_loss],
-                      ', RTD ms:', [format(1000 * x, '.4g') for x in round_trip_delay],
-                      '> st.dev ms:', [format(1000 * x, '.4g') for x in st_dev]
+                      'Loss %:', format(100 * packet_loss, '.4g'),
+                      ', RTD ms:', format(1000 * round_trip_delay, '.4g'),
+                      '> st.dev ms:', format(1000 * st_dev, '.4g')
                       )
 
-                print('\n'+self.getName()+': The tested ended above as I received no data for 5 seconds.\n')
+                print('\n'+self.getName()+': The test ended above as I received no data for 5 seconds.\n')
                 break
 
 
 class Sending(threading.Thread):
 
-    def __init__(self, sock, dst_ip, dst_udp_port, test_duration_mins=1):
+    def __init__(self, sock, dst_ip, dst_udp_port, tos=0, test_duration=60):
         self.sock = sock
         self.dest_ip = dst_ip
         self.dest_udp_port = dst_udp_port
-        self.test_duration_minutes = test_duration_mins
+        self.tos = tos
+        self.test_duration = test_duration
         threading.Thread.__init__(self)
 
     def unauthenticated_test_packet(self, seq, padding=1400):  # https://tools.ietf.org/html/rfc4656#section-4.1.2
@@ -215,12 +174,12 @@ class Sending(threading.Thread):
 
     def run(self):
         # While loop
-        packet_seq = 0
-        packet_ipp = 0
+        packet_seq = 1000000 * self.tos
         test_sample = 0
-        while True:
-            self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_TOS, packet_ipp * 32)  # Set IP ToS Byte
 
+        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_TOS, self.tos)  # Set IP ToS Byte
+
+        while True:
             # Create Layer4 payload for the TWAMP Unauthenticated Test packet
             msg = self.unauthenticated_test_packet(packet_seq)
 
@@ -229,18 +188,15 @@ class Sending(threading.Thread):
 
             packets_sent_queue.put(packet_seq, block=False)  # Send this info to "Listening" thread
 
-            time.sleep(0.120)  # Current thread will sleep for 120 msec
+            time.sleep(0.100)  # Current thread will sleep for 100 msec
 
             packet_seq += 1
-            packet_ipp += 1
-            if packet_ipp == 8:
-                test_sample += 1  # Increment after 8 test pckts sent, with ToS: 0, 32, 64, 96, 128, 160, 192, 224
-                packet_ipp = 0
+            test_sample += 1
 
-            if test_sample == 60 * self.test_duration_minutes:  # Test sample increases every 800ms (due to sleep) so
-                # this if statement defines Test runtime.
-                # Default value (1) equals to 1 minute runtime.
+            if test_sample == 10 * self.test_duration:  # Test sample increases every 100ms (due to sleep) so
+                # this if statement defines Test runtime. Default value (60) equals to 1 minute runtime.
                 break
+
 
 # -- Main --
 def Main():
